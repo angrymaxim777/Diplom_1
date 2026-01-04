ff4d0fdc756a476382744332e988a867d3fdf9e5
Max <angrymaxim777@gmail.com>
2026-01-05 02:17:17 +0400

    add testSetBuns to BurgerTest

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -1,4 +1,49 @@
 package praktikum;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 public class BurgerTest {
+
+    @Mock
+    private Bun bunMock;
+
+    @Mock
+    private Ingredient ingredientMock1;
+
+    @Mock
+    private Ingredient ingredientMock2;
+
+    @Mock
+    private Ingredient ingredientMock3;
+
+    private Burger burger;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        burger = new Burger();
+    }
+
+    @Test
+    public void testSetBuns() {
+        when(bunMock.getName()).thenReturn("black bun");
+        when(bunMock.getPrice()).thenReturn(100.0f);
+
+        burger.setBuns(bunMock);
+
+        assertSame(bunMock, burger.bun);
+    }
+
 }
